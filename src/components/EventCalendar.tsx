d5d20ce75c229e36577292732c058ab488d8b170
Mohammad Imran <raining101290@gmail.com>
2026-02-01 19:14:05 +0600

    initial setup and bug fixed

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,26 +1,26 @@
-"use client";
+'use client';
 
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const EventCalendar = () => {
-  const [value, onChange] = useState<Value>(new Date());
+   const [value, onChange] = useState<Value>(new Date());
 
-  const router = useRouter();
+   const router = useRouter();
 
-  useEffect(() => {
-    if (value instanceof Date) {
-      router.push(`?date=${value}`);
-    }
-  }, [value, router]);
+   useEffect(() => {
+      if (value instanceof Date) {
+         router.push(`?date=${value}`);
+      }
+   }, [value, router]);
 
-  return <Calendar onChange={onChange} value={value} />;
+   return <Calendar onChange={onChange} value={value} />;
 };
 
 export default EventCalendar;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,43 +3,43 @@
 // IN THE TUTORIAL WE'RE TAKING THE NEXT WEEK AS THE REFERENCE WEEK.
 
 const getLatestMonday = (): Date => {
-  const today = new Date();
-  const dayOfWeek = today.getDay();
-  const daysSinceMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
-  const latestMonday = today;
-  latestMonday.setDate(today.getDate() - daysSinceMonday);
-  return latestMonday;
+   const today = new Date();
+   const dayOfWeek = today.getDay();
+   const daysSinceMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
+   const latestMonday = today;
+   latestMonday.setDate(today.getDate() - daysSinceMonday);
+   return latestMonday;
 };
 
 export const adjustScheduleToCurrentWeek = (
-  lessons: { title: string; start: Date; end: Date }[]
+   lessons: { title: string; start: Date; end: Date }[]
 ): { title: string; start: Date; end: Date }[] => {
-  const latestMonday = getLatestMonday();
+   const latestMonday = getLatestMonday();
 
-  return lessons.map((lesson) => {
-    const lessonDayOfWeek = lesson.start.getDay();
+   return lessons.map((lesson) => {
+      const lessonDayOfWeek = lesson.start.getDay();
 
-    const daysFromMonday = lessonDayOfWeek === 0 ? 6 : lessonDayOfWeek - 1;
+      const daysFromMonday = lessonDayOfWeek === 0 ? 6 : lessonDayOfWeek - 1;
 
-    const adjustedStartDate = new Date(latestMonday);
+      const adjustedStartDate = new Date(latestMonday);
 
-    adjustedStartDate.setDate(latestMonday.getDate() + daysFromMonday);
-    adjustedStartDate.setHours(
-      lesson.start.getHours(),
-      lesson.start.getMinutes(),
-      lesson.start.getSeconds()
-    );
-    const adjustedEndDate = new Date(adjustedStartDate);
-    adjustedEndDate.setHours(
-      lesson.end.getHours(),
-      lesson.end.getMinutes(),
-      lesson.end.getSeconds()
-    );
+      adjustedStartDate.setDate(latestMonday.getDate() + daysFromMonday);
+      adjustedStartDate.setHours(
+         lesson.start.getHours(),
+         lesson.start.getMinutes(),
+         lesson.start.getSeconds()
+      );
+      const adjustedEndDate = new Date(adjustedStartDate);
+      adjustedEndDate.setHours(
+         lesson.end.getHours(),
+         lesson.end.getMinutes(),
+         lesson.end.getSeconds()
+      );
 
-    return {
-      title: lesson.title,
-      start: adjustedStartDate,
-      end: adjustedEndDate,
-    };
-  });
+      return {
+         title: lesson.title,
+         start: adjustedStartDate,
+         end: adjustedEndDate,
+      };
+   });
 };
